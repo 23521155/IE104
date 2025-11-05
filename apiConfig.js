3a5430c33db2581483047bf549289150f63af2cf
Phi <23521155@gm.uit.edu.vn>
2025-11-06 00:18:57 +0700

    update v1.4.1

--- apiConfig.js
+++ apiConfig.js
@@ -1,5 +1,6 @@
 export const API_CONFIG = {
     BASE_URL: "http://localhost:1234/v1",
-    DEPLOY_URL: "http://54.179.196.246:1234/v1",
+    DEPLOY_URL: "https://ie104-be-cs2z.onrender.com/v1",
+    DEPLOY_URL_AWS: "http://54.179.196.246:1234/v1",
 };
 export const FE_URL = `https://ie104.onrender.com`;
